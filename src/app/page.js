690ed7ef9c1105448d90332e6b28f40783d4a745
Ahamed Asif <138349258+ahamedasif07@users.noreply.github.com>
2025-09-24 21:15:20 +0600

    hero done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Hero from "@/components/Hero/Hero";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="px-[15px] py-[10px] sm:px-[80px] md:px-[120px] md:py-[19px] sm:py-[15px]">
-      home
+      <Hero />
     </div>
   );
 };
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Image from "next/image";
+import StorImage from "../../../public/images/Frame 2147227442.png";
+import UnderLine from "../../../public/images/Vector 7621.png";
+import VictorPng from "../../../public/images/Vector.png";
+import HeroImage from "../../../public/images/Rectangle 161124256.png";
+
+export default function Hero() {
+  return (
+    <section className="relative  from-white ">
+      <div className="    py-5 grid grid-cols-1 lg:grid-cols-2 items-center place-content-between">
+        {/*---------------------------- Left Content --------------------------------*/}
+        <div className="text-center lg:text-left relative ">
+          <h1 className="text-[40px] md:text-[60px] font-extrabold text-[#212B36] leading-[62px] tracking-wide relative inline-block">
+            All Your Jobs <br />
+            <Image
+              src={VictorPng}
+              alt="underline"
+              width={100}
+              height={70}
+              className="absolute right-[170px] -top-[40px] -z-10"
+            />
+            <span className="relative inline-block">
+              One Smart App
+              {/* Decorative PNG under text */}
+              <Image
+                src={UnderLine}
+                alt="underline"
+                width={290}
+                height={70}
+                className="absolute right-0 bottom-[-10px] -z-10"
+              />
+            </span>
+          </h1>
+
+          <p className="mt-6 text-[16px] text-[#637381] max-w-lg mx-auto lg:mx-0">
+            Built for business owners, employees, and clients to streamline job
+            scheduling, service tracking, and team management in one powerful
+            app.
+          </p>
+
+          {/* ---------------------- Store Buttons ---------------------- */}
+          <div className="mt-[38px] flex justify-center lg:justify-start gap-4">
+            <a
+              href="#"
+              className="relative w-[150px] h-[45px] md:w-[310px] md:h-[49px]"
+            >
+              <Image
+                src={StorImage}
+                alt="App Store"
+                fill
+                className="object-contain"
+              />
+            </a>
+          </div>
+        </div>
+
+        {/*---------------------- Right Image--------------------------- */}
+        <div className="relative w-full max-w-[650px] h-[400px]  md:h-[500px]  lg:h-[600px] flex justify-center lg:justify-end">
+          <Image
+            src={HeroImage}
+            alt="App Preview"
+            fill
+            className="object-fill drop-shadow-2xl"
+            priority
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
